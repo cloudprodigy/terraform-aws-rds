9f1e5d256f79b9e613b547c341009d85f245f158
ricky <ricky@cloudprodigy.ca>
2022-11-15 10:16:26 -0500

    updated name prefix for sm and parameter group

--- parameter_group.tf
+++ parameter_group.tf
@@ -17,9 +17,10 @@ resource "aws_db_parameter_group" "default" {
 }
 
 resource "aws_rds_cluster_parameter_group" "default" {
-  count  = local.engine == "aurora-postgresql" || local.engine == "aurora-mysql" ? 1 : 0
-  name   = lower(var.app_name)
-  family = var.db_family
+  count = local.engine == "aurora-postgresql" || local.engine == "aurora-mysql" ? 1 : 0
+  #name   = lower(var.app_name)
+  name_prefix = var.app_name
+  family      = var.db_family
 
   dynamic "parameter" {
     for_each = var.cluster_db_parameters
